d338672fba5cdf5414f420d8e20bf0d6b449adfb
student <student@students-Mac.local>
2020-11-18 01:21:57 -0800

    last commit

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var playerscore = 0
+    var cpuscore  = 0
+    @IBOutlet weak var message: UILabel!
+    
+    @IBOutlet weak var CPUCard: UIImageView!
+    @IBOutlet weak var action_button: UIButton!
+    @IBOutlet weak var PlayerCard: UIImageView!
+    @IBOutlet weak var CPUScore: UILabel!
+      @IBOutlet weak var PlayerScore: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //generate values 2-14
+    func generateValue()->Int{
+        return Int.random(in: 2...14)    }
+    
+    //function action for Deal button
+    @IBAction func DealFunc(_ sender: UIButton) {
+       
+        var playernumber=generateValue()
+        var cpunumber=generateValue()
+        PlayerCard.image = UIImage (imageLiteralResourceName: "card\(playernumber)")
+        CPUCard.image = UIImage (imageLiteralResourceName: "card\(cpunumber)")
+        if playernumber>cpunumber {
+            playerscore += 1
+            message.text = "Player wins the round"
+            PlayerScore.text = String(playerscore)
+            CPUScore.text = String(cpuscore)
+        }
+        else if playernumber<cpunumber{
+            cpuscore += 1
+            message.text = "cpu wins the round"
+            PlayerScore.text = String(playerscore)
+            CPUScore.text = String(cpuscore)
+        }
+        else {
+            playerscore += 1
+            cpuscore += 1
+            message.text = "draw"
+            PlayerScore.text = String(playerscore)
+            CPUScore.text = String(cpuscore)
+        }
+        
+        
+        
+    
+    }
+    
+    
 }
 
